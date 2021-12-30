9ae2be8d585f3dc9eb393cb2e7918611a062025f
daimaxiaxie <zhang971207@hotmail.com>
2021-12-30 10:51:51 +0800

    v 0.1.1

--- pallets/dns/src/lib.rs
+++ pallets/dns/src/lib.rs
@@ -203,6 +203,7 @@ pub mod pallet {
 	impl<T: Config> Pallet<T> {
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+		/*
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
 		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
@@ -239,6 +240,7 @@ pub mod pallet {
 			}*/
 			Ok(())
 		}
+		*/
 
 		#[pallet::weight(1_010_000 + T::DbWeight::get().reads_writes(1,2)) ]
 		pub fn register(origin: OriginFor<T>, mut domain: [u8; 32], duration: u128) -> DispatchResult {
